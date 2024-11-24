8e92a6933badb616d1d8167b06ae7ab9b3279777
Korneliia <korneliiaPositive@gmail.com>
2024-11-24 17:59:33 +0100

    nav styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChatPageComponent } from './pages/chat-page/chat-page.component';
-import { ChatBoxComponent } from './pages/chat-page/chat-box/chat-box.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ChatPageComponent} from './pages/chat-page/chat-page.component';
+import {ChatBoxComponent} from './pages/chat-page/chat-box/chat-box.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MaxLengthPipe} from './pipes/max-length.pipe';
@@ -13,26 +13,28 @@ import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatCheckbox} from '@angular/material/checkbox';
 import {FormsModule} from '@angular/forms';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-import { MainBannerComponent } from './pages/main-page/main-banner/main-banner.component';
+import {MainPageComponent} from './pages/main-page/main-page.component';
+import {MainBannerComponent} from './pages/main-page/main-banner/main-banner.component';
 import {NgIconsModule} from '@ng-icons/core';
 import {faEye} from '@ng-icons/font-awesome/regular';
-import { OurPartnersComponent } from './pages/main-page/our-partners/our-partners.component';
-import { AiDocumentationComponent } from './pages/main-page/ai-documentation/ai-documentation.component';
-import { SmoothScrollComponent } from './smooth-scroll/smooth-scroll.component';
-import { FooterComponent } from './pages/main-page/footer/footer.component';
-import { AccessibilityComponent } from './fetures/accessibility/accessibility.component';
+import {OurPartnersComponent} from './pages/main-page/our-partners/our-partners.component';
+import {AiDocumentationComponent} from './pages/main-page/ai-documentation/ai-documentation.component';
+import {SmoothScrollComponent} from './smooth-scroll/smooth-scroll.component';
+import {FooterComponent} from './pages/main-page/footer/footer.component';
+import {AccessibilityComponent} from './fetures/accessibility/accessibility.component';
 import {NgxMarqueeModule} from 'ngx-marquee';
 import {TranslatePipe, TrPipe} from './pipes/translate.pipe';
 import {TranslateService} from './pipes/translate.service';
 import {HttpClientModule} from '@angular/common/http';
+import {faBrandFacebook, faBrandInstagram, faBrandTwitter} from '@ng-icons/font-awesome/brands';
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
     AppComponent,
-    ChatPageComponent,MaxLengthPipe,
+    ChatPageComponent, MaxLengthPipe,
     ChatBoxComponent,
-    MainPageComponent,TranslatePipe,
+    MainPageComponent, TranslatePipe,
     MainBannerComponent,
     OurPartnersComponent, TrPipe,
     AiDocumentationComponent,
@@ -45,18 +47,18 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     MatIcon,
     MatIconButton,
-    MatRadioGroup,HttpClientModule,
+    MatRadioGroup, HttpClientModule,
     MatRadioButton,
     MatPaginator, NgxMarqueeModule,
     MatCheckbox,
     MatButton,
     FormsModule,
-    NgIconsModule.withIcons({faEye})
+    NgIconsModule.withIcons({faEye, faBrandInstagram, faBrandTwitter, faBrandFacebook})
   ],
   providers: [
     provideAnimationsAsync(),
     TranslateService
-  ],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
