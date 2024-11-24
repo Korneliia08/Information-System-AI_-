c3fee0bf89e1b30c6728eb2c92cb9058d6a10aff
AlfaGruisPL <alfagruis@gmail.com>
2024-11-24 12:00:41 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import $ from 'jquery'; // Import jQuery
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular';
+
+  ngOnInit(){
+    // @ts-ignore
+    $(".marquee").marquee({
+      duration: 11000,
+      gap: 50,
+      delayBeforeStart: 0,
+      direction: "left",
+      duplicated: true,
+    });
+
+    // @ts-ignore
+    $(".bigmarquee").marquee({
+      duration: 11000,
+      gap: 50,
+      delayBeforeStart: 0,
+      direction: "left",
+      duplicated: true,
+    });
+
+    // @ts-ignore
+    $(".bigmarqueeSlow").marquee({
+      duration: 18000,
+      gap: 50,
+      delayBeforeStart: 0,
+      direction: "left",
+      duplicated: true,
+    });
+  }
 }
