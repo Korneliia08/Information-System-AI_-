cbdfefc186cc1f89078b6df2ee75a5e8e6c19f58
Korneliia Mushak <jackiemaistrenko@gmail.com>
2025-02-26 23:54:42 +0100

    send

--- src/app/pages/user-cabinet-page/panels/user-cabinet-dashboard-page/user-cabinet-stat-block1/user-cabinet-stat-block1.component.ts
+++ src/app/pages/user-cabinet-page/panels/user-cabinet-dashboard-page/user-cabinet-stat-block1/user-cabinet-stat-block1.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {NgStyle} from '@angular/common';
-import {faSolidExclamation} from '@ng-icons/font-awesome/solid';
+import {faSolidCheck, faSolidExclamation, faSolidInfo} from '@ng-icons/font-awesome/solid';
 import {NgIcon} from '@ng-icons/core';
+import {remixCloseFill, remixVipDiamondLine} from '@ng-icons/remixicon';
 
 @Component({
   selector: 'app-user-cabinet-stat-block1',
@@ -11,12 +12,32 @@ import {NgIcon} from '@ng-icons/core';
   styleUrl: './user-cabinet-stat-block1.component.scss'
 })
 export class UserCabinetStatBlock1Component {
-  public lines = Array(7).fill(null).map(() => ({
-    color: 'red',
+  public lines = [{
+    color: '#f63333',
     icon: faSolidExclamation,
-    value: Math.random() * 100, // Startowa wartość losowa
-    direction: Math.random() > 0.5 ? 1 : -1 // Losowy kierunek początkowy
-  }));
+    value: Math.random() * 100,
+    direction: Math.random() > 0.5 ? 1 : -1
+  },{
+    color: '#488ffa',
+    icon: remixVipDiamondLine,
+    value: Math.random() * 100,
+    direction: Math.random() > 0.5 ? 1 : -1
+  },{
+    color: '#fa3ce4',
+    icon: remixCloseFill,
+    value: Math.random() * 100,
+    direction: Math.random() > 0.5 ? 1 : -1
+  },{
+    color: '#9b31ff',
+    icon: faSolidInfo,
+    value: Math.random() * 100,
+    direction: Math.random() > 0.5 ? 1 : -1
+  },{
+    color: '#27d77a',
+    icon: faSolidCheck,
+    value: Math.random() * 100,
+    direction: Math.random() > 0.5 ? 1 : -1
+  },];
 
   protected readonly faSolidExclamation = faSolidExclamation;
   protected readonly Math = Math;
