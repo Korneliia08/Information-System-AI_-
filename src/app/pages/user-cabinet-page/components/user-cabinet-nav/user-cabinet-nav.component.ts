3e37183d0eb95bb6148b22aec4ca4912006b894b
AlfaGruisPL <alfagruis@gmail.com>
2025-02-28 00:46:03 +0100

    init

--- src/app/pages/user-cabinet-page/components/user-cabinet-nav/user-cabinet-nav.component.ts
+++ src/app/pages/user-cabinet-page/components/user-cabinet-nav/user-cabinet-nav.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {AnimationOptions, LottieComponent} from 'ngx-lottie';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {UserCabinetNavGroupComponent} from './user-cabinet-nav-group/user-cabinet-nav-group.component';
-import {faSolidPerson} from '@ng-icons/font-awesome/solid';
+import {faSolidDesktop, faSolidLock, faSolidNetworkWired, faSolidPerson, faSolidShield} from '@ng-icons/font-awesome/solid';
 
 @Component({
   selector: 'app-user-cabinet-nav',
@@ -24,4 +24,9 @@ export class UserCabinetNavComponent {
 
 
   protected readonly faSolidPerson = faSolidPerson;
+  protected readonly faSolidDesktop = faSolidDesktop;
+
+  protected readonly faSolidNetworkWired = faSolidNetworkWired;
+  protected readonly faSolidLock = faSolidLock;
+  protected readonly faSolidShield = faSolidShield;
 }
